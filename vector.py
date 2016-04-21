e1fa37dbf71698335e5b8ff9052e2dd2c6a3b6ab
vivekvinodh <vvino001@ucr.edu>
2016-04-21 01:34:23 -0700

    Added vector subtraction fn

--- vector.py
+++ vector.py
@@ -19,6 +19,10 @@ class Vector(object):
             new_coordinates.append(self.coordinates[i] + v.coordinates[i])
         return Vector(new_coordinates)
 
+    def minus(self, v):
+        new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
